2efa5a1a4ca84fd14c35bc930a9a1d045c04205d
Chay-May <chayenne.mayara@gmail.com>
2022-08-08 22:31:45 -0300

    Exercícios de fixação off 013

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,25 @@
-const num = //seu numero aqui
+//Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
 
+const num = prompt ("Insira seu número aqui") //seu numero aqui
+
+
+// 1. Utilizando ifs aninhados
+/*
+if (num%2 == 0) {
+    console.log ("Seu número é divisível por 2");
+ if (num%3 == 0) {
+    console.log("Seu número é divisível por 3")
+} else {
+    console.log ("Seu número não é divisível por 3")
+}
+} else {
+    console.log ("Seu número não é divisível por 2")
+}
+*/
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+
+if (num%2 == 0 || num%3 == 0) {
+    alert("Seu número é divisível")
+} else {
+    alert("Seu número não é divisível")
+}
